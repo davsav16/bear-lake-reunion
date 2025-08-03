5d625201861ace1d29450df033521112d88532a7
davidmcdougal <davidbyu6@gmail.com>
2025-08-03 16:46:03 -0600

    going to add constants

---
+++ app/constants/familymembers.ts
@@ -0,0 +1,11 @@
+export const familyMembers = [
+  { name: "Brent and Nancy" },
+  {
+    name: "Troy and Mary",
+  },
+  { name: "Ron and Faith" },
+  { name: "Sandy and Joe" },
+  { name: "Cheri and Ryan" },
+  { name: "Larry and Anet" },
+  { name: "Randy and Jaqui" },
+];
